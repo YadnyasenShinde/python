d1e465c53318650b16fc8155637b646a6e7d566a
Yadnyasen Shinde <119351522+YadnyasenShinde@users.noreply.github.com>
2023-10-06 23:57:04 +0530

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -1,2 +1,19 @@
-print('Hii')
-grades= [33,44,67,46,26]
+# Get the user's name
+name = input("What's your name? ")
+
+# Get the user's age
+age = int(input("How old are you? "))
+
+# Greet the user
+print(f"Hello, {name}!")
+
+# Provide a personalized message based on age
+if age < 18:
+    print("You are quite young!")
+elif age >= 18 and age < 30:
+    print("You are in your prime!")
+else:
+    print("You have a lot of life experience!")
+
+# Additional message
+print("Thank you for using this program.")
