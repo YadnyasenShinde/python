62a6ff7eb0ddc725147ea03813af2e21538b5129
Yadnyasen Shinde <119351522+YadnyasenShinde@users.noreply.github.com>
2023-10-08 00:42:01 +0530

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -5,12 +5,12 @@ name = input("What's your name? ")
 age = int(input("How old are you? "))
 
 # Greet the user
-print(f"Hi, {name}!")
+print(f"Hii, {name}!")
 
 # Provide a personalized message based on age
 if age < 18:
     print("You are quite young!")
-elif age >= 18 and age < 30:
+elif age >= 18 and age < 32:
     print("You are in your prime!")
 else:
     print("You have a lot of life experience!")
