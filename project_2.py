d1c152d765615edfadc01fa8bbd42e0ae92cbe36
Yadnyasen Shinde <119351522+YadnyasenShinde@users.noreply.github.com>
2023-10-11 00:12:25 +0530

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -5,3 +5,15 @@ def remove_vowels(input_string):
 user_input = input()
 result = remove_vowels(user_input)
 print( result)
+
+
+# Function to remove vowels
+def rem_vowel(string):
+    vowels = ['a','e','i','o','u']
+    result = [letter for letter in string if letter.lower() not in vowels]
+    result = ''.join(result)
+    print(result)
+
+# Take user input
+string = input("Enter a string: ")
+rem_vowel(string)
