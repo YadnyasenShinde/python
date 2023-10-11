1b51afb4c6fc6507649ed2b8158be5078a6c7a69
Yadnyasen Shinde <119351522+YadnyasenShinde@users.noreply.github.com>
2023-10-12 00:25:42 +0530

    Create pattern_code.py

---
+++ pattern_code.py
@@ -0,0 +1,10 @@
+n = 7
+for i in range(n):
+    spaces = n - i - 1
+    stars = 2 * i + 1
+    print(" " * spaces + "*" * stars)
+
+for i in range(n - 2, -1, -1):
+    spaces = n - i - 1
+    stars = 2 * i + 1
+    print(" " * spaces + "*" * stars)
